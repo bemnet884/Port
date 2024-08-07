6e824cfb9df56a0b1faab4e1e25b3603a63ef8cb
bemnet884 <155265449+bemnet884@users.noreply.github.com>
2024-08-07 11:03:08 -0700

    chnages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-function App() {
+import { BrowserRouter } from "react-router-dom";
+
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+
+const App = () => {
   return (
-    <div className="App">
-      Hello from bemnet 
-    </div>
+    <BrowserRouter>
+      <div className="realtive z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero/>
+        </div>
+        <div>
+          <About />
+          <Experience />
+          <Tech />
+          <Works />
+          <Feedbacks />
+        </div>
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas/>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
