a5b8bdb7a16653857fc5643ee1442902998b21e8
bemnet884 <155265449+bemnet884@users.noreply.github.com>
2024-08-07 12:44:48 -0700

    c

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,12 @@
 import { Suspense, useEffect, useState } from 'react';
-
-
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
 const Computers = () => {
+  const computer = useGLTF("./desktop_pc/scene.gltf");
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black"/>
+    </mesh>
   )
 }
 
